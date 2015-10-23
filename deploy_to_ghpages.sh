88be8b4ad27900cdc8ac614024ae2a9a195b1b4c
Gerald Pape <ubergesundheit@users.noreply.github.com>
2015-10-23 16:09:56 +0200

    Update deploy_to_ghpages.sh

--- deploy_to_ghpages.sh
+++ deploy_to_ghpages.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
--x
+PDF_SHA=$(curl -u "$GITHUB_AUTH" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/git/trees/gh-pages:output" | python -c 'import sys, json; print json.load(sys.stdin)["tree"][0]["sha"]')
 
-echo "$TRAVIS_REPO_SLUG"
-
-#PDF_SHA=$(curl -u "$GITHUB_AUTH" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/git/trees/gh-pages:output" | python -c 'import sys, json; print json.load(sys.stdin)["tree"][0]["sha"]')
-
-#curl -u "$GITHUB_AUTH" -H "Accept: application/vnd.github.v3+json" -X PUT -d @- "https://api.github.com/repos/$TRAVIS_REPO_SLUG/contents/output/erstiheft.pdf" <<CURL_DATA
-#{
-#  "message": "update pdf",
-#  "sha": "$PDF_SHA",
-#  "committer": {
-#    "name": "travis",
-#    "email": "travis@travis-ci.org"
-#  },
-#  "branch": "gh-pages",
-#  "content": "$(base64 erstiheft_gesamt.pdf)"
-#}
-#CURL_DATA
+curl -u "$GITHUB_AUTH" -H "Accept: application/vnd.github.v3+json" -X PUT -d @- "https://api.github.com/repos/$TRAVIS_REPO_SLUG/contents/output/erstiheft.pdf" <<CURL_DATA
+{
+  "message": "update pdf",
+  "sha": "$PDF_SHA",
+  "committer": {
+    "name": "travis",
+    "email": "travis@travis-ci.org"
+  },
+  "branch": "gh-pages",
+  "content": "$(base64 erstiheft_gesamt.pdf)"
+}
+CURL_DATA
 
